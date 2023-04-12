0a1dc47482daea0e22e0f7f737a66a813161ac13
DnZ19 <dennis@centerofagile.nl>
2023-04-12 17:02:24 +0200

    feature achter de ADMIN gezet om functionaliteit netjes te maken, kan nu gebruikers en hun rollen opvragen

---
+++ src/main/java/com/novi/eindopdracht/recipeDiary/recipeDiary/service/UserService.java
@@ -0,0 +1,34 @@
+package com.novi.eindopdracht.recipeDiary.recipeDiary.service;
+
+import com.novi.eindopdracht.recipeDiary.recipeDiary.dto.UserCredentialsDto;
+import com.novi.eindopdracht.recipeDiary.recipeDiary.model.Role;
+import com.novi.eindopdracht.recipeDiary.recipeDiary.model.User;
+import com.novi.eindopdracht.recipeDiary.recipeDiary.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserRepository uRepos;
+
+
+    public UserService(UserRepository uRepos) {
+        this.uRepos = uRepos;
+    }
+
+    public List<UserCredentialsDto> getAllUserCredentials(){
+        List<User> users = new ArrayList<>();
+        uRepos.findAll().forEach(users::add);
+
+        return users.stream()
+                .map(user -> new UserCredentialsDto(user.getUsername(), user.getRoles().stream().map(Role::getRolename).collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+    }
+
+
+}
